6f0b5e3951d0d2bc7e7934385280b23e71e4225f
NABU Bremerhaven-Wesermünde <61091241+nabubremerhaven@users.noreply.github.com>
2020-09-18 19:13:07 +0200

    Update and rename maptest_3.js to maptest_4.js

---
+++ maptest_4.js
@@ -0,0 +1,5 @@
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmFidS1icmVtZXJoYXZlbiIsImEiOiJja2Y4aHhxaTcwNDV2MzJsZDhwejFhM20zIn0.Ji2CAaQFXkmIb5Q7DNJx3w',{maxZoom: 18,attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap<\/a> contributors, ' +'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA<\/a>, ' +
+'Imagery © <a href="https://www.mapbox.com/">Mapbox<\/a>',id: 'mapbox/streets-v11',tileSize: 512,zoomOffset: -1}).addTo(mymap);
+L.marker([51.5, -0.09]).addTo(mymap);
+L.marker.bindPopup("<b>Hello world!<\/b><br>I am a popup.").openPopup();
